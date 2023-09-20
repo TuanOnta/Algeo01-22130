22182c777b28a666696a977403869b87a3cc2a32
Onta <mdz54dzakiarta@gmail.com>
2023-09-21 00:09:57 +0700

    Update membuat beberapa operasi matrix serta fungsi createTranspose dan createIdentity

--- src/matrix/matrix.java
+++ src/matrix/matrix.java
@@ -37,7 +37,7 @@ public class Matrix {
         return (m.numCols -1);
     }
 
-    // Assignment Matrix
+    // Baca/tulis Matrix
     public static void readMatrix(Matrix m,int baris, int kolom){
 
         System.out.println("Masukan elemen-elemen matrix: ");
@@ -57,6 +57,7 @@ public class Matrix {
         }
     }
 
+    // Operasi assignment Matrix
     public static void copyMatrix(Matrix m,Matrix mcopy){
         for (int i=0;i<m.numRows;i++){
             for(int j=0;j<m.numCols;j++){
@@ -65,7 +66,7 @@ public class Matrix {
         }
     }
 
-    // Operasi Matrix
+    // Operasi Aritmatika terhadap matrix
     public static Matrix addMatrix(Matrix m1, Matrix m2){
         // IS : matrix m1 dan m2 memiliki jumlah baris dan kolom yang sama
         // FS : Mengirim hasil penjumlahan m1 + m2
@@ -109,7 +110,60 @@ public class Matrix {
             }
         }
         return m;
-
     }
 
+    // Operasi Relational
+    public static boolean isMatrixEqual(Matrix m1, Matrix m2){
+        if (m1.numRows == m2.numRows && m1.numCols == m2.numCols){
+            for (int i=0;i<m1.numRows;i++){
+                for(int j=0;j<m1.numCols;j++){
+                    if (m1.mem[i][j] != m2.mem[i][j]){
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isMatrixNotEqual(Matrix m1, Matrix m2){
+        return (!isMatrixEqual(m1,m2));
+    }
+
+    public static boolean isMatrixSizeEqual(Matrix m1, Matrix m2){
+        return (m1.numRows == m2.numRows && m1.numCols == m2.numCols);
+    }
+
+    public static int countElmt(Matrix m){
+        return (m.numRows * m.numCols);
+    }
+    
+    // Membuat matrix unik
+    public static Matrix createIdentity(int n){
+        // Membuat matrix identitas dengan panjang baris dan kolom adalah n
+        Matrix result = new Matrix(n,n);
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if (i==j){
+                    result.mem[i][j] = 1;
+                }else{
+                    result.mem[i][j] = 0;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static Matrix createTranspose(Matrix m){
+        Matrix result = new Matrix(m.numCols,m.numRows);
+
+        for(int i=0;i<m.numRows;i++){
+            for(int j=0;j<m.numCols;j++){
+                result.mem[j][i] = m.mem[i][j];
+            }
+        }
+        return result;
+    }
 }
