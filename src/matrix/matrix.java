df5712c5a44037ac9d8e1f5d92217300e4a8ab12
Onta <mdz54dzakiarta@gmail.com>
2023-09-20 20:01:25 +0700

    first commit, createMatrix and printMatrix

--- src/matrix/matrix.java
+++ src/matrix/matrix.java
@@ -1,6 +1,31 @@
+package Matrix;
+
 import java.util.Scanner;
 
-public class Matrix{
+public class matrix {
     public static Scanner sc;
-    public static void 
+
+    public static void createMatrix(double[][] matrix, int baris, int kolom){
+        double mark = -9999;
+        int i,j;
+        matrix = new double[baris][kolom];
+        
+        for (i=0;i < baris;i++){
+            for (j=0;j<kolom;j++){
+                matrix[i][j] = mark;
+            }
+        }
+    }
+
+    public static void printMatrix(double[][] matrix){
+        int i,j;
+        for(j = 0; j <matrix.length;j++){
+            for (i = 0; i < matrix[j].length;j++){
+                System.out.print(matrix[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
 }
